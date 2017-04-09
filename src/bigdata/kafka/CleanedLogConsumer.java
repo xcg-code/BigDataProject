eb2c1341d38d99eaee85f2eb199d21b77944cae1
xcg-code <1450152983@qq.com>
2017-04-09 10:37:31 +0800

    基本项目完成

--- src/bigdata/kafka/CleanedLogConsumer.java
+++ src/bigdata/kafka/CleanedLogConsumer.java
@@ -22,7 +22,7 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.message.Message;
 import kafka.message.MessageAndMetadata;
 
-public class LogConsumer {
+public class CleanedLogConsumer {
 
 	public static void main(String[] args) {
 		Properties prop=new Properties();
@@ -49,8 +49,11 @@ public class LogConsumer {
 			while(it.hasNext()){
 				MessageAndMetadata<byte[], byte[]> entry=it.next();
 				String msg=new String(entry.message());
-				String path="/user/hanson/raw/"+Util.getDate(msg)+"/a.log";
-				writeLog(msg,path);
+				String path="/user/hanson/hive/warehouse"+Util.getDate(msg)+"/a.log";
+				msg=cleanData(msg);
+				if(msg!=null){
+					writeLog(msg,path);
+				}
 				String key="Null";
 				if(entry.key()!=null){
 					key=new String(entry.key());
@@ -69,12 +72,28 @@ public class LogConsumer {
 			Configuration conf=new Configuration();
 			conf.set("fs.defaultFS", "hdfs://hanson01:8020");
 			FileSystem fs=FileSystem.get(conf);
-			FSDataOutputStream out=fs.create(new Path(path));
+			FSDataOutputStream out=fs.append(new Path(path));
 			out.writeChars(line+"\r\n");
 			out.hsync();
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static String cleanData(String line){
+		String[] arr=line.split(",");
+		String requestLine=null;
+		String[] requestSplit=null;
+		String request=null;
+		if(arr!=null&&arr.length>5){
+			requestLine=arr[3];
+			requestSplit=requestLine.split(" ");
+			request=requestSplit[1];
+			if(request.endsWith(".html")||request.endsWith(".htm")){
+				return line;
+			}
+		}
+		return null;
 	}
 }
